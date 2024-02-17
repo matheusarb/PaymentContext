4b908e0761acb90c8038e37c851d7a15ba9ad248
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-17 14:47:26 -0300

    limiting modify access to Entity

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -2,6 +2,8 @@ namespace PaymentContext.Domain;
 
 public class Student
 {
+    private IList<Subscription> _subscriptions = new List<Subscription>();
+
     public Student(string firstName, string lastName, string document, string email)
     {
         FirstName = firstName;
@@ -11,11 +13,19 @@ public class Student
     }
 
     public string FirstName { get; private set; }
-    public string LastName { get; private set; }  
-    public string Document { get; private set; }  
-    public string Email { get; private set; } 
+    public string LastName { get; private set; }
+    public string Document { get; private set; }
+    public string Email { get; private set; }
     public string Address { get; private set; }
 
-    public List<Subscription> Subscriptions { get; set; }
+    public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
 
+    public void AddSubscription(Subscription subscription)
+    {
+        //cancelar todas as assinaturas existentes e ativar a nova
+        foreach (var sub in Subscriptions)
+            sub.IsActive = false;
+
+        _subscriptions.Add(subscription);
+    }
 }
